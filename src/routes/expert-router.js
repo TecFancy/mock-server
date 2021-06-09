b340737674227aad6745c378c7425b9f7a8bdd6b
王若非 <wangruofei@inter-credit.net>
2021-06-09 17:03:35 +0800

    chore: 模拟接口

--- src/routes/expert-router.js
+++ src/routes/expert-router.js
@@ -8,5 +8,7 @@ router.prefix("/api/mock");
 router.get("/expert/getSingleExpertInfo", expert.getSingleExpertInfo);
 router.get("/expert/settlementMethodEmun", expert.settlementMethodEmun);
 router.post("/expert/reportOrderList", expert.reportOrderList);
+router.get("/expert/getOrderStatusList", expert.getOrderStatusList);
+router.post("/expert/getOrderSettlementData", expert.getOrderSettlementData);
 
 module.exports = router;
